af1a2e8ab6da6a78a72c867ed6c5d91c23e8a10e
Deepak Lakshmipathi <deepak.lakshmipathi550@gmail.com>
2026-03-06 22:21:11 +0530

    task: initial Terraform scaffold

---
+++ terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "aws_region" {
+  description = "AWS region to deploy to"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "environment" {
+  description = "Deployment environment"
+  type        = string
+  default     = "dev"
+}
